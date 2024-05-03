7029e56a5b23f1822a95b3571dbeeae18509ddc4
Sygams <angelosumagaysay15@gmail.com>
2024-05-04 01:11:47 +0800

    First Commit

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:+63 907 698 2876"><i class="fas fa-phone"></i> +63 907 698 2876</a>
+         <a href="tel:+63 966 397 3398"><i class="fas fa-phone"></i> +63 966 397 3398</a>
+         <a href="mailto:abbygail@gmail.com"><i class="fas fa-envelope"></i> abbygailbarber@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/abbygail.barber" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Abbygail A. Barber</span> | all rights reserved!</div>
+
+</footer>
